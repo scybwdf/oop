921be1d5c6fc4ce4af71448cf907c87c28e6c608
root <root@felix.localdomain>
2018-01-02 17:31:12 +0800

    新文件：    config.php 	修改：      mysql.class.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$cfg=array(
+    'HOST'=>'localhost',
+    'USER'=>'root',
+    'PASSWORD'=>'dakehui9118',
+    'DB'=>'oop',
+    'ex'=>'wf_',
+    'charset'=>'UTF8'
+);
+
+return $cfg;
+
+?>
--- mysql.class.php
+++ mysql.class.php
@@ -1,42 +1,160 @@
 <?php
 
-class Mysql{
+abstract class aDB{
+
+    /**
+     * 连接数据库，从配置文件读取配置信息
+     */
+    abstract public function conn();
+
+    /**
+     * 执行sql
+     * @param $sql
+     * @return mixed
+     */
+    abstract public function query($sql);
+
+    /**获取所有记录
+     * @param $sql
+     * @return array
+     */
+    abstract public function getAll($sql);
+
+    /**
+     * 获取一行记录
+     * @param $sql
+     * @return mixed
+     */
+    abstract public function getRow($sql);
+
+    /**
+     * 获取一条记录
+     * @param $sql
+     * @return mixed
+     */
+    abstract public function getOne($sql);
+
+    /**
+     * 自动创建sql 并执行
+     * @param $data 关联数组 键/值与表中列/值对应
+     * @param $table 表名称
+     * @param string $act 动作 insert/update
+     * @param string $where 条件
+     * @param int return 新插入行的主键值或行数
+     */
+    abstract public function Exec($data,$table,$act='insert',$where='0');
+
+    /**
+     * 获取最后插入的id
+     * @return mixed
+     */
+    abstract public function lastId();
+
+    /**
+     * 获取sql影响行数
+     * @return mixed
+     */
+    abstract public function affectRows();
+
+}
+
+class Mysql extends aDB {
     public $link;
 
     public function __construct()
     {
         $this->conn();
+
+
     }
 
+    /**
+     * 连接数据库，从配置文件读取配置信息
+     */
     public function conn(){
-        $config=array(
-            'host'=>'localhost',
-            'user'=>'root',
-            'password'=>'dakehui9118',
-            'db'=>'wf',
-            'charset'=>'utf8'
-        );
-        $this->link=mysqli_connect($config['host'],$config['user'],$config['password'],$config['db']);
-        mysqli_query("set names {$config['charset']}",$this->link);
-        return $this->link;
+        $cfg=include "config.php";
+        $this->link=new mysqli($cfg['HOST'],$cfg['USER'],$cfg['PASSWORD'],$cfg['DB']);
+        $this->query('set names '.$cfg['charset']);
     }
 
-    public function query($sql){
-        return mysqli_query($this->link,$sql);
-    }
+    /**
+     * 执行sql
+     * @param $sql
+     * @return mixed
+     */
+     public function query($sql){
+        return $this->link->query($sql);
+     }
 
+    /**获取所有记录
+     * @param $sql
+     * @return array
+     */
     public function getAll($sql){
-
-        $data=array();
         $res=$this->query($sql);
-        while($row=mysqli_fetch_assoc($res)){
-            $data[]=$row;
-         }
+        $data=[];
+        while($re=$res->fetch_assoc()){
+            $data[]=$re;
+        }
+        return $data;
+    }
+
+    /**
+     * 获取一行记录
+     * @param $sql
+     * @return mixed
+     */
+    public function getRow($sql){
+        $res=$this->query($sql);
+        $data=$res->fetch_assoc();
+        return $data;
+    }
+
+    /**
+     * 获取一条记录
+     * @param $sql
+     * @return mixed
+     */
+    public function getOne($sql){
+        $res=$this->query($sql);
+        $data=$res->fetch_row()[0];
         return $data;
 
     }
-}
-$mysql=new Mysql();
-var_dump($mysql->getAll("select * from wf_active"));
 
+    /**
+     * 自动创建sql 并执行
+     * @param $data 关联数组 键/值与表中列/值对应
+     * @param $table 表名称
+     * @param string $act 动作 insert/update
+     * @param string $where 条件
+     * @param int return 新插入行的主键值或行数
+     */
+    public function Exec($data,$table,$act='insert',$where='0'){
+        if($act=='insert'){
+            $sql='insert into '.$table.' (';
+            $sql.=implode(',',array_keys($data)).") values ('";
+            $sql.=implode("','",array_values($data))."')";
+        }
+        else{
+
+            $sql="update {$table} set ";
+            foreach ($data as $k=>$v){
+                $sql.=$k."='".$v."',";
+            }
+            $sql=rtrim($sql,',');
+            $sql.=" where $where";
+
+        }
+        return $this->query($sql);
+    }
+
+    public function lastId(){
+        return $this->link->insert_id;
+    }
+
+    public function affectRows(){
+        return $this->link->affected_rows;
+    }
+}
 ?>
